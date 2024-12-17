befbd8350bf4e83c6fb45955de71b453be1daa4a
kimgyeongmo <mohunk1989@gmail.com>
2024-12-17 14:55:26 +0900

    style: 반려사유 저장시 버튼 수정

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1239,7 +1239,7 @@ export default function OrderList() {
             </button>
             <button
               onClick={handleRejectionSubmit}
-              className="px-4 py-2 bg-red-500 text-white rounded"
+              className="px-4 py-2 bg-blue-500 text-white rounded"
             >
               저장
             </button>
