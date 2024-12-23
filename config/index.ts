eabd9dfdcd8a61d83962b002e07605ebf8befcd9
kimgyeongmo <mohunk1989@gmail.com>
2024-12-23 11:41:56 +0900

    chore: API_BASE_URL 변경

--- config/index.ts
+++ config/index.ts
@@ -1,2 +1,5 @@
 // Backend API configuration
-export const API_BASE_URL = 'http://localhost:8000';
+// export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = 'https://samsip-be.koyeb.app';
+
+
