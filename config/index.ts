3ae3953cfad958744d7a8af882b90973cff9fa1a
kimgyeongmo <mohunk1989@gmail.com>
2025-01-11 11:39:34 +0900

    chore: 서버URL 로컬에서 코옙으로 변경

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 // Backend API configuration
-export const API_BASE_URL = 'http://localhost:8000';
-// export const API_BASE_URL = 'https://samsip-be.koyeb.app';
+// export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = 'https://samsip-be.koyeb.app';
 
 
